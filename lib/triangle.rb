845c8312c4480816e15ab1c4594f95475d56b911
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-09-05 18:44:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,11 +20,9 @@ class Triangle
       end
     end
   end
-  
-  class TriangleError < StandardError
-  # triangle error code
-  end
 end
 
+class TriangleError < StandardError
+  # triangle error code
+  end
 
-#

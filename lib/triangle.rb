0eafec09276b5eda1811f333a4cf334bcd997fec
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-09-05 18:35:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,11 @@ class Triangle
   end
   
   def kind 
-    if Triangle.new(side1 == side2 == side3)
-      :equalateral
+    if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
   end
 end

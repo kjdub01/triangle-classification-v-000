d9dd4a0d6dad108a20e91dab0aa8db6ea1cbc5e1
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-09-05 18:48:32 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,8 @@
 class Triangle
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
   
 def kind()
